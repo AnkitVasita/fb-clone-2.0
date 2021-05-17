659a411637fee34155acda74e1878dff2c75536f
AnkitVasita <vasitaankit97@gmail.com>
2021-05-17 21:52:34 +0530

    ADD:header

---
+++ components/Header.js
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+  FlagIcon,
+  SearchIcon,
+  PlayIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
+
+const Header = () => {
+  return (
+    <div className="flex items-center sticky top-0 z-50 bg-white p-1 lg:px-5 shadow-md">
+      {/* left */}
+      <div className="flex items-center">
+        <Image
+          src="http://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+      </div>
+      <div className="flex  items-center ml-2 rounded-full bg-gray-100 p-2 text-gray-600">
+        <SearchIcon className="h-6 " />
+        <input
+          className=" hidden md:inline-flex flex ml-2 items-center bg-transparent  outline-none placeholder-gray-500 flex-shrink "
+          type="text"
+          placeholder="Search Facebook"
+        />
+      </div>
+
+      {/* center */}
+      <div className="flex justify-center flex-grow ">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon Icon={HomeIcon} active />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
+
+      {/* right */}
+      <div className="flex items-center justify-end sm:space-x-2">
+        <p className=" whitespace-nowrap font-semibold  pr-3 ">Ankit Vasita</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
